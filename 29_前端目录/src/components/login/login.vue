074a8f0c5c3c667ab929c07ae81fd0649977e937
Wjianting <376356057@qq.com>
2020-03-27 15:42:21 +0800

    login页面

--- 29_前端目录/src/components/login/login.vue
+++ 29_前端目录/src/components/login/login.vue
@@ -1,8 +1,53 @@
 <template>
+	<div class="login-wrap">
+		<el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+			<h2>用户登录</h2>
+			<el-form-item label="用户名">
+				<el-input v-model="formdata.username"></el-input>
+			</el-form-item>
+			<el-form-item label="密码">
+				<el-input v-model="formdata.password"></el-input>
+			</el-form-item>
+			<el-button class='login-btn' type="primary">登录</el-button>
+			<el-button class='forget-btn' type="primary">忘记密码</el-button>
+		</el-form>
+	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				formdata: {
+					username: '',
+					password: ''
+				}
+			}
+		}
+	}
 </script>
 
 <style>
+	.login-wrap {
+		height: 100%;
+		background-color: #324152;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.login-wrap .login-form {
+		background-color: #fff;
+		width: 400px;
+		padding: 30px;
+		border-radius: 5px;
+	}
+
+	.login-wrap .login-form .login-btn {
+		margin-top: 20px;
+	}
+
+	.login-wrap .login-form .forget-btn {
+		margin-left:172px;
+	}
 </style>
--- 29_前端目录/src/router/index.js
+++ 29_前端目录/src/router/index.js
@@ -3,48 +3,67 @@ import vueRouter from 'vue-router'
 import Container from '@/components/Container.vue'
 Vue.use(vueRouter)
 
-const routes = [
+const routes = [{
+		path: '/',
+		component: () => import('@/components/login/Login.vue'),
+		children: [{
+			name: 'login',
+			path: '/login',
 
+
+		}]
+	},
 	{
-		path:'/',
-		component:Container,
-		children:[
-			{
-				path:'/home',
-				component:()=>import('@/components/home/Home.vue')
-			}
-		]
+		path: '/home',
+		component: Container,
+		children: [{
+			path: '/home',
+			component: () => import('@/components/home/Home.vue')
+		}]
 	},
 	{
 		path: '/errPage',
 		component: Container,
 		hidden: false,
-		meta: {title: '异常页', icon: 'rfq', noCache: true},
+		meta: {
+			title: '异常页',
+			icon: 'rfq',
+			noCache: true
+		},
 		children: [{
-				path: '/errPage/404',
-				component: () => import('@/components/errPage/Page404.vue'),
-				name: 'errPage404',
-				hidden: false,
-				meta: {title: '找不到页面', noCache: true}
+			path: '/errPage/404',
+			component: () => import('@/components/errPage/Page404.vue'),
+			name: 'errPage404',
+			hidden: false,
+			meta: {
+				title: '找不到页面',
+				noCache: true
+			}
 		}, {
-				path: '/errPage/403',
-				name: 'errPage403',
-				hidden: false,
-				meta: {title: '无权访问', noCache: true},
-				component: () => import('@/components/errPage/Page403.vue')
+			path: '/errPage/403',
+			name: 'errPage403',
+			hidden: false,
+			meta: {
+				title: '无权访问',
+				noCache: true
+			},
+			component: () => import('@/components/errPage/Page403.vue')
 		}, {
-				path: '/errPage/500',
-				name: 'errPage500',
-				hidden: false,
-				meta: {title: '服务器出错', noCache: true},
-				component: () => import('@/components/errPage/Page500.vue')
+			path: '/errPage/500',
+			name: 'errPage500',
+			hidden: false,
+			meta: {
+				title: '服务器出错',
+				noCache: true
+			},
+			component: () => import('@/components/errPage/Page500.vue')
 		}]
 	}
 ]
 
 const router = new vueRouter({
 	routes,
-	mode:'history'
+	mode: 'history'
 })
 
 export default router

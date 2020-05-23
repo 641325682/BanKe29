6dc91d6d9e1ff7b6321d1d5f841aec9873cb3099
Wjianting <376356057@qq.com>
2020-05-23 08:08:52 +0800

    前端目录

--- 29_前端目录/src/views/About.vue
+++ 29_前端目录/src/views/About.vue
@@ -2,27 +2,27 @@
   <div class="about">
     <h1>第22组</h1>
     <el-alert
-            title="庄金惠 180327099"
+            title=""
             type="success"
             :closable="false">
     </el-alert>
     <el-alert
-            title="刘永裕 180327111"
+            title=""
             type="success"
             :closable="false">
     </el-alert>
     <el-alert
-            title="贺  翎 180327103"
+            title=""
             type="success"
             :closable="false">
     </el-alert>
     <el-alert
-            title="庄  震 180327120"
+            title=""
             type="success"
             :closable="false">
     </el-alert>
     <el-alert
-            title="朱伟大 180327097"
+            title=""
             type="success"
             :closable="false">
     </el-alert>
